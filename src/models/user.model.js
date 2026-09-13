e6d262a22461bf420a1cfe950d9c518008733703
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-13 23:02:16 +0530

    token and model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -60,4 +60,30 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect= async function (password) {
     return await bcrypt.compare(password, this.password)
 }
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {_id: this._id,
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:rocess.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+},
 export const User = mongoose.model('User', userSchema);
