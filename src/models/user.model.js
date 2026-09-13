1c544d7dc05817723a291d4d4cf4c7902ae9ec08
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-13 22:44:07 +0530

    token and modelling

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
     {
         username: {
@@ -44,7 +45,19 @@ const userSchema = new Schema(
             type: String
         }
     }
+    {
+        timestamps: true
+    }
 )
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect= async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
 export const User = mongoose.model('User', userSchema);
