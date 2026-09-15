246d12db6df83b9a9e62adee9713f26a7c296bd2
aryanchoudhary001 <aryanchoudhary0526@gmail.com>
2026-09-15 22:15:34 +0530

    huhh

--- src/app.js
+++ src/app.js
@@ -4,22 +4,24 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    Credentials: true
-}))
+    credentials: true,
+  })
+)
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended  : true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
-//routes
+// routes
 import userRouter from "./routes/user.routes.js"
 
-//routes decleration
-app.use("/api/v1/users",userRouter)
+// routes declaration
+app.use("/api/v1/users", userRouter)
+// http://localhost:5000/api/v1/users/register
 
-http://localhost:8000/api/v1/users/register
-export{app}
+
+export { app }
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,4 +7,7 @@ const registerUser = asyncHandler(async (req, res)=>{
     })
 })
 
-export {registerUser}
+export {
+    registerUser,
+
+}
